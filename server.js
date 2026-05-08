5c062c9d3cac007f9ff9dff0851da4b2eeb235a3
Iamzohaibali <j36740062@gmail.com>
2026-05-08 11:40:25 +0500

    Initial

---
+++ server.js
@@ -0,0 +1,13 @@
+// server.js
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("CI/CD Working");
+});
+
+app.listen(3000, () => {
+  console.log("Server running");
+});
+
+
